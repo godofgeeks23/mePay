0cb723e46e79178978d4e8d208c4e07cb312e49c
Aviral Srivastava <aviralji4@gmail.com>
2023-05-24 22:12:08 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,70 +1,72 @@
 let login = document.getElementById("login");
 let signup = document.getElementById("signup");
-let profile = document.getElementById('profile');
-let profileLogo = document.getElementById('profileLogo');
-let walletSwitch = document.getElementById('wallet-switch');
-let bankSwitch = document.getElementById('bank-switch');
-let wallet = document.getElementById('wallet-transfer-container');
-let bank = document.getElementById('bank-transfer-container');
+let profile = document.getElementById("profile");
+let profileLogo = document.getElementById("profileLogo");
+let walletSwitch = document.getElementById("wallet-switch");
+let bankSwitch = document.getElementById("bank-switch");
+let wallet = document.getElementById("wallet-transfer-container");
+let bank = document.getElementById("bank-transfer-container");
 
 //reseting the browser storage value
 let proceedFromHomeButtonOnly = false;
-sessionStorage.setItem('proceedFromHomeButtonOnly', proceedFromHomeButtonOnly);
+sessionStorage.setItem("proceedFromHomeButtonOnly", proceedFromHomeButtonOnly);
 
 let faceVerified = false;
-sessionStorage.setItem('faceVerified', faceVerified);
+sessionStorage.setItem("faceVerified", faceVerified);
 
 let payFromFacePageOnly = false;
-sessionStorage.setItem('payFromFacePageOnly', payFromFacePageOnly);
+sessionStorage.setItem("payFromFacePageOnly", payFromFacePageOnly);
 
 const clickHomeNextBtn = () => {
-    proceedFromHomeButtonOnly = true;
-    sessionStorage.setItem('proceedFromHomeButtonOnly', proceedFromHomeButtonOnly);
-    window.location.href = './src/face-verification.html';
-}
+  proceedFromHomeButtonOnly = true;
+  sessionStorage.setItem(
+    "proceedFromHomeButtonOnly",
+    proceedFromHomeButtonOnly
+  );
+  window.location.href = "./src/face-verification.html";
+};
 
 const clickLogoImg = () => {
-    window.location.href = './index.html';
-}
- 
+  window.location.href = "./index.html";
+};
+
 login.onclick = () => {
-    window.location.href = './src/login.html';
-}
+  window.location.href = "./src/login.html";
+};
 
 signup.onclick = () => {
-    window.location.href = './src/signup.html';
-}
+  window.location.href = "./src/signup.html";
+};
 
 profile.onclick = () => {
-    window.location.href = './src/profile.html';
-}
+  window.location.href = "./src/profile.html";
+};
 
 // switching of wallet and bank section
 walletSwitch.onclick = () => {
-    wallet.classList.replace('hide', 'unhide');
-    bank.classList.replace('unhide', 'hide');
-}
+  wallet.classList.replace("hide", "unhide");
+  bank.classList.replace("unhide", "hide");
+};
 
 bankSwitch.onclick = () => {
-    wallet.classList.replace('unhide', 'hide');
-    bank.classList.replace('hide', 'unhide');
-}
-
+  wallet.classList.replace("unhide", "hide");
+  bank.classList.replace("hide", "unhide");
+};
 
 // setting the profile picture based on user login or not
 let currentUser = null;
-let keepLoggedIn = localStorage.getItem("keepLoggedIn"); 
+let keepLoggedIn = localStorage.getItem("keepLoggedIn");
 function getUserName() {
-    if (keepLoggedIn == "yes") {
-        currentUser = JSON.parse(localStorage.getItem('user'));
-    } else {
-        currentUser = JSON.parse(sessionStorage.getItem('user'));
-    }
+  if (keepLoggedIn == "yes") {
+    currentUser = JSON.parse(localStorage.getItem("user"));
+  } else {
+    currentUser = JSON.parse(sessionStorage.getItem("user"));
+  }
 }
 getUserName();
 if (currentUser.profileImgURL != "null") {
-    let photoURL = currentUser.profileImgURL;
-    profileLogo.setAttribute('src', photoURL);
+  let photoURL = currentUser.profileImgURL;
+  profileLogo.setAttribute("src", photoURL);
 } else {
-    profileLogo.setAttribute('src', "./images/profileM.jpg");
+  profileLogo.setAttribute("src", "./images/profileM.jpg");
 }
